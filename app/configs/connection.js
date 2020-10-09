d2e330ae40604da4c16eba047f7351e9e84f9204
jackie <mvpbremah@yahoo.com>
2020-10-09 00:46:26 -0400

    commit

--- app/configs/connection.js
+++ app/configs/connection.js
@@ -1,37 +1,42 @@
-const Connection = require("mysql2/typings/mysql/lib/Connection");
+//const Connection = require("mysql2/typings/mysql/lib/Connection");
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
+const mysql = require("mysql");
+let connection;
+
 
 // Dependencies
 var Sequelize = require("sequelize");
 
 if (process.env.JAWSDB_URL) {
-  Connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
   
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+  
     user: "root",
-    password: "hacktheplanet",
-    database: "todoagain_db"
+    password: "Bremen12345$",
+    database: "battler_db"
   }) 
 }
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("battler_db", "root", "", {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  dialect: "mysql",
-  database: "geo_battle",
-  password: "Delarosa93",
-  database: "battler_db",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// var sequelize = new Sequelize("battler_db", "root", "", {
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   dialect: "mysql",
+//   database: "geo_battle",
+//   password: "Bremen12345$",
+//   database: "battler_db",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+//
 
 // Exports the connection for other files to use
-module.exports = sequelize;
+connection.connect();
+module.exports = connection;
